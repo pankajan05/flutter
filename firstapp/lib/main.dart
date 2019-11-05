ff17d0ef1422a0cc577ff521927b8898d32c4d43
Pankajan Satkunam <pankajan05@gmail.com>
2019-11-05 00:24:42 -0800

    add some picture for the app

--- firstapp/lib/main.dart
+++ firstapp/lib/main.dart
@@ -5,12 +5,17 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('EasyList'),
         ),
+        body: Card(child: Column(children: <Widget>[
+          Image.asset('assets/food.jpg'),
+          Text('Food Paradise')
+        ],)),
       ),
     );
   }
