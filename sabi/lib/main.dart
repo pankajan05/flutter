3b0a649aae1240dab7f3e3df7caabdc431668321
Pankajan Satkunam <pankajan05@gmail.com>
2019-12-14 11:44:23 +0530

    add drawer to the app

--- sabi/lib/main.dart
+++ sabi/lib/main.dart
@@ -1,23 +1,56 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  return runApp(SabiApp());
-}
+void main() => runApp(SabiApp());
 
-class SabiApp extends StatelessWidget{
+
+class SabiApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return MaterialApp(
-      home:  Scaffold(appBar: AppBar(
-        
-        title: Text("Sabinaya",),),
-        body: Center(child: RaisedButton(
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Sabinaya",
+        ),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: const <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text(
+                'Pankajan',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.message),
+              title: Text('Messages'),
+            ),
+            ListTile(
+              leading: Icon(Icons.account_circle),
+              title: Text('Photos'),
+            ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text('Settings'),
+            ),
+          ],
+        ),
+      ),
+      body: Center(
+        child: RaisedButton(
           onPressed: null,
           child: Text("Press me"),
         ),
-      ),)
-    );
+      ),
+      
+    ));
   }
-
 }
