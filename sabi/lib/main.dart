dd9c7df2156e71277af3f5b0080c5dbb1bcad08c
Pankajan Satkunam <pankajan05@gmail.com>
2019-12-14 14:23:31 +0530

    add image when click button

--- sabi/lib/main.dart
+++ sabi/lib/main.dart
@@ -2,55 +2,78 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(SabiApp());
 
-
 class SabiApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-      appBar: AppBar(
-        title: Text(
-          "Sabinaya",
-        ),
-      ),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: const <Widget>[
-            DrawerHeader(
-              decoration: BoxDecoration(
-                color: Colors.blue,
-              ),
-              child: Text(
-                'Pankajan',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 24,
-                ),
+            appBar: AppBar(
+              title: Text(
+                "Sabinaya",
               ),
             ),
-            ListTile(
-              leading: Icon(Icons.message),
-              title: Text('Messages'),
+            drawer: Drawer(
+              child: ListView(
+                padding: EdgeInsets.zero,
+                children: const <Widget>[
+                  DrawerHeader(
+                    decoration: BoxDecoration(
+                      color: Colors.blue,
+                    ),
+                    child: Text(
+                      'Pankajan',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 24,
+                      ),
+                    ),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.message),
+                    title: Text('Messages'),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.account_circle),
+                    title: Text('Photos'),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.settings),
+                    title: Text('Settings'),
+                  ),
+                ],
+              ),
             ),
-            ListTile(
-              leading: Icon(Icons.account_circle),
-              title: Text('Photos'),
-            ),
-            ListTile(
-              leading: Icon(Icons.settings),
-              title: Text('Settings'),
-            ),
-          ],
-        ),
-      ),
-      body: Center(
-        child: RaisedButton(
-          onPressed: null,
-          child: Text("Press me"),
-        ),
-      ),
-      
-    ));
+            body: MainStatefulWidget()));
+  }
+}
+
+class MainStatefulWidget extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MainStatefulWidgetState();
+  }
+}
+
+class MainStatefulWidgetState extends State {
+  String count = '';
+  
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child:Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Image.network('$count'),
+        Text('Number', style: TextStyle(fontSize: 50)),
+        RaisedButton(
+          onPressed: () {
+            setState(() {
+            count = 'https://i.pinimg.com/280x280_RS/1c/53/d5/1c53d5b3502165cb8d49cfcb246b9e30.jpg'; 
+            });
+          },
+          child: Text('increment'),
+        )
+      ],
+    ),);
   }
 }
