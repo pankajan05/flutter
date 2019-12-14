a305cfcdf6ebe7ca35fc6a0f596e2a04670ffed1
Pankajan Satkunam <pankajan05@gmail.com>
2019-12-14 10:00:04 +0530

    create a stateless widget

---
+++ sabi/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+
+}
+
+class SabiApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Text("Hi my love Sabi");
+  }
+
+}
