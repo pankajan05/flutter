979f8ce4de3bfa28298de00cf78deaf30f93a677
Pankajan Satkunam <pankajan05@gmail.com>
2019-12-14 10:36:19 +0530

    add text

--- sabi/lib/main.dart
+++ sabi/lib/main.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
 
 void main() {
-
+  return runApp(SabiApp());
 }
 
 class SabiApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Text("Hi my love Sabi");
+    return MaterialApp(
+      home: Center(
+      child: Text("Hi my love Sabi",
+      style: TextStyle(color : Colors.red),
+      ),
+      ),
+    );
   }
 
 }
