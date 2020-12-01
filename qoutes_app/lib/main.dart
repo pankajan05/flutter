96b36b72a281a428af56c84f14d95153fc601801
pankajan <pankajan05@gmail.com>
2020-12-01 11:54:04 +0530

    learn the list

--- qoutes_app/lib/main.dart
+++ qoutes_app/lib/main.dart
@@ -1,9 +1,37 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MateialApp(
+  runApp(MaterialApp(
+    home: QuoteList(),
   ));
 }
 
+class QuoteList extends StatefulWidget {
+  @override
+  _QuoteListState createState() => _QuoteListState();
+}
 
+class _QuoteListState extends State<QuoteList> {
+  List<String> quotes= [
+    'Accept the Truth or Live with the lies',
+  'We are What we think',
+  'Our life is our design',
+  'Yesterday is past, Tomorrow is future, Today is gift that is why we call it present'
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        title: Text('Quotes'),
+        centerTitle: true,
+        backgroundColor: Colors.green,
+      ),
+      body: Column(
+        children: quotes.map((quote) => Text(quote)).toList(),
+      ),
+    );
+  }
+}
 
