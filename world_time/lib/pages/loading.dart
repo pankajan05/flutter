f6a4a4a518781aca4622422d0117c50ba30a5666
pankajan <pankajan05@gmail.com>
2020-12-06 19:41:31 +0530

    create the world time service

--- world_time/lib/pages/loading.dart
+++ world_time/lib/pages/loading.dart
@@ -1,6 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
-import 'dart:convert';
+import 'package:world_time/services/world_time.dart';
+
 
 class Loading extends StatefulWidget {
   @override
@@ -8,24 +9,32 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
+  String time = 'loading';
 
-  void getData() async{
-    Response response = await get('https://jsonplaceholder.typicode.com/todos/1');
-    Map data = jsonDecode(response.body);
-    print(data);
-    print(data['title']);
+  void setupWorldTime() async{
+    WorldTime instance = WorldTime(location: 'Colombo', flag: 'Srilanka.png', url: 'Asia/Colombo');
+
+    await instance.getTime();
+    setState(() {
+      time = instance.time;
+    });
   }
 
   void initState() {
     super.initState();
-    getData();
-
+    setupWorldTime();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('loading'),
+      appBar: AppBar(
+        title: Text('Loading'),
+      ),
+        body: Padding(
+          padding: EdgeInsets.all(50.0),
+          child: Text(time),
+        ),
     );
   }
 
---
+++ world_time/lib/services/world_time.dart
@@ -0,0 +1,25 @@
+import 'package:http/http.dart';
+import 'dart:convert';
+
+class WorldTime{
+   String location;
+   String time;
+   String flag;
+   String url;
+
+
+   WorldTime({this.location, this.flag, this.url});
+
+  Future<void> getTime() async{
+     Response response = await get('http://worldtimeapi.org/api/timezone/$url');
+     Map data = jsonDecode(response.body);
+     String datetime = data['datetime'];
+     String offset = data['utc_offset'];
+
+     DateTime now = DateTime.parse(datetime);
+     now.add(Duration(hours: int.parse(offset.substring(1,3)), minutes: int.parse(offset.substring(4,6))));
+     this.time = now.toString();
+
+   }
+}
+
