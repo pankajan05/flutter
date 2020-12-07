69f7da215b651b9a4a9ea76217fdd0db0d328100
pankajan <pankajan05@gmail.com>
2020-12-07 20:48:15 +0530

    add a loading icon

--- world_time/lib/pages/loading.dart
+++ world_time/lib/pages/loading.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:world_time/services/world_time.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 
 class Loading extends StatefulWidget {
@@ -29,12 +30,12 @@ class _LoadingState extends State<Loading> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Loading'),
-      ),
-        body: Padding(
-          padding: EdgeInsets.all(50.0),
-          child: Text("loading"),
+      backgroundColor: Colors.blue,
+        body: Center(
+          child: SpinKitWanderingCubes(
+            color: Colors.white,
+            size: 50,
+          ),
         ),
     );
   }
